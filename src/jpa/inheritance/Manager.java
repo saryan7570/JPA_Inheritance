3216d3458c989b5960cb7c2a622fe886595d1070
saryan7570 <52561240+saryan7570@users.noreply.github.com>
2019-08-09 17:38:27 +0530

    Add files via upload

---
+++ src/jpa/inheritance/Manager.java
@@ -0,0 +1,20 @@
+package jpa.inheritance;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mgr_store")
+public class Manager extends Employee {
+	@Column(length = 10)
+	private String departmentName;
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+}
